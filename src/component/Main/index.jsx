b7d15920e8d9f5bb8858edd281a60e087ed5b63f
Alunos <aluno.etec@etecEmbu.com.br>
2022-09-09 21:52:33 -0300

    Novo Commit

--- src/component/Main/index.jsx
+++ src/component/Main/index.jsx
@@ -4,10 +4,11 @@ import ImgNoir from '../img/noir.png'
 import ImgRhino from '../img/rhino.png'
 import ImgOhara from '../img/ohara.png'
 import ImgVenom from '../img/venom.png'
+import './style.css'
 
 function index() {
   return (
-    <div>
+    <div className="Main">
       <Card className="Noir" imagem={ImgNoir}/>
       <Card className="Rhino" imagem={ImgRhino}/>
       <Card className="Ohara"imagem={ImgOhara}/>
